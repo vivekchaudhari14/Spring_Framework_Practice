fa8b9b289b5e4d744d1a1c3fbea2d8bab078e1e6
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-05-31 12:01:21 +0530

    Insert Value into Collection Using @value

---
+++ src/main/java/stereotype/CollectionValue.java
@@ -0,0 +1,26 @@
+package stereotype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component
+public class CollectionValue {
+	@Value("#{temp}")
+	List<Integer> rollNumber;
+
+	public List<Integer> getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(List<Integer> rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionValue [rollNumber=" + rollNumber + "]";
+	}
+	
+	
+}
