0f6742030b24130e172072e0be0d384a918cdcd0
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-20 15:06:55 +0530

    Add Model Obj

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,12 +1,30 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+	@RequestMapping("/home")
+	public String getModel(Model model) {
+		System.out.println("This is home url");
+		model.addAttribute("name", "Vivek");
+		model.addAttribute("id",55);
+		List<String> friends = new ArrayList<String>();
+		friends.add("vivek");
+		friends.add("Harshal");
+		friends.add("mayur");
+		friends.add("Uday");
+		model.addAttribute("friends",friends);
+		return "index";
+	}
+	
+	@RequestMapping("/")
     public String home() {
         return "index";
     }
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,5 @@
+<%@page import="org.apache.jasper.tagplugins.jstl.core.ForEach"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -10,6 +12,22 @@
 	<h1>This is Spring controller</h1>
 	<h1>called by Controller</h1>
 	<h1>url  .home</h1>
-	<h2>My name is vivek</h2>
+	<% 
+		String name = (String) request.getAttribute("name");
+		Integer id = (Integer) request.getAttribute("id");
+		List<String> friends = (List<String>) request.getAttribute("friends");
+	%>
+	<h2>My name is <%=name %></h2>
+	<h2>My id Number is <%=id %></h2>
+	
+	<%
+    	for (String f : friends) {
+	%>
+
+	<h2>My friend is <%= f %></h2>
+
+	<%
+	    }
+	%>
 </body>
 </html>
